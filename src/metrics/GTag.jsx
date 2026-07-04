e60544b205bb87aa75d1999570bd8043b1eb9cac
ct-amit <phoenixcoded20@outlook.com>
2026-07-04 10:41:19 +0530

    fixed ga metrics script

--- src/metrics/GTag.jsx
+++ src/metrics/GTag.jsx
@@ -1,40 +1,34 @@
 "use client"
 
-import { useEffect } from "react"
 import PropTypes from "prop-types"
 
-export function loadGTag(gaId) {
+import Script from "next/script"
+
+/***************************  METRICS - G-TAG  ***************************/
+
+// No visible UI for this component
+export default function GTag({ gaId }) {
   if (!gaId) {
     console.warn("Google Analytics ID is missing.")
     return
   }
 
-  // Check if the script already exists
-  if (document.getElementById("gtag-script")) return
-
-  // Must be defined before the script loads so GA can queue commands immediately
-  window.dataLayer = window.dataLayer || []
-  window.gtag = function (...args) {
-    window.dataLayer.push(args)
-  }
-  window.gtag("js", new Date())
-  window.gtag("config", gaId)
-
-  // Add the Google Analytics script
-  const script = document.createElement("script")
-  script.id = "gtag-script"
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`
-  script.async = true
-  document.head.appendChild(script)
-}
-
-export default function GTag({ gaId }) {
-  useEffect(() => {
-    // Call the utility function directly
-    loadGTag(gaId)
-  }, [gaId])
-
-  return null // No visible UI for this component
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}');
+          `}
+      </Script>
+    </>
+  )
 }
 
 GTag.propTypes = { gaId: PropTypes.string }
